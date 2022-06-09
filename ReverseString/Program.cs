44c844c2e1800b750fac0c32580470acca2f46bf
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-09 22:04:10 +0200

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter the word to reverse :");
+            string word = Console.ReadLine();
+
+            char[] cArray = word.ToCharArray();
+            string wordReverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                wordReverse += cArray[i];
+            }
+            Console.WriteLine("Your reverse word is: " + wordReverse);
+        }
+    }
+}
